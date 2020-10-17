1e68714ad34c06b92de8efba303f9c6e2fa241eb
Alexandre de Souza <alexandre@aledsz.com.br>
2020-10-17 17:15:51 -0300

    Remove docs

--- apps/char_server/lib/char_server.ex
+++ apps/char_server/lib/char_server.ex
@@ -2,17 +2,4 @@ defmodule CharServer do
   @moduledoc """
   Documentation for CharServer.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CharServer.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
